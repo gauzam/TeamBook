94d28df201c231cb477aed4b5f5b7978e4cd3617
gauzam <gautam01428@gmail.com>
2023-12-13 22:22:11 +0530

    Updated EmployeeDAOImpl

--- src/main/java/com/springboot/TeamBook/dao/EmployeeDAOImpl.java
+++ src/main/java/com/springboot/TeamBook/dao/EmployeeDAOImpl.java
@@ -30,4 +30,35 @@ public class EmployeeDAOImpl implements EmployeeDAO{
         //return the result list
         return employees;
     }
+
+    @Override
+    public Employee findById(int theId) {
+
+        //get the employee by id
+        Employee theEmployee = entityManager.find(Employee.class, theId);
+
+        //return the employee
+        return theEmployee;
+    }
+
+    @Override
+    public Employee save(Employee theEmployee) {
+
+        //save or update the employee (depends on the id)
+        Employee dbEmployee = entityManager.merge(theEmployee);
+
+        //return the saved or updated employee
+        return dbEmployee;
+    }
+
+    @Override
+    public void deleteById(int theId) {
+
+        //find the employee by id
+        Employee theEmployee = entityManager.find(Employee.class, theId);
+
+        //delete the employee
+        entityManager.remove(theEmployee);
+
+    }
 }
