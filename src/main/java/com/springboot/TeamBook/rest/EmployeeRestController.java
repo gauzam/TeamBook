a32cd069496d4bafdcd493b4bbf2874e77ba4498
gauzam <gautam01428@gmail.com>
2023-12-16 13:13:41 +0530

    Update EmployeeRestController
    Added GET and POST mappings

--- src/main/java/com/springboot/TeamBook/rest/EmployeeRestController.java
+++ src/main/java/com/springboot/TeamBook/rest/EmployeeRestController.java
@@ -3,9 +3,7 @@ package com.springboot.TeamBook.rest;
 import com.springboot.TeamBook.entity.Employee;
 import com.springboot.TeamBook.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,6 +24,34 @@ public class EmployeeRestController {
     @GetMapping("/employees")
     public List<Employee> findAll(){
         return employeeService.findAll();
+    }
+
+    // add mapping for GET /employees/{employeeId}
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId){
+
+        Employee theEmployee = employeeService.findById(employeeId);
+
+        if(theEmployee == null){
+            throw new RuntimeException("Employee id not found: " + employeeId);
+        }
+
+        return theEmployee;
+    }
+
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee theEmployee){
+
+        //just in case they pass an id in json... set it to 0
+        //this is to force the insert of the new item... instead of update
+
+        theEmployee.setId(0);
+
+        Employee dbEmployee = employeeService.save(theEmployee);
+
+        return dbEmployee;
+
+
     }
 
 
