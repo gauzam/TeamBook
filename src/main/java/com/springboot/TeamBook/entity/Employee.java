010772ab20cb3d23510fb49b91da096379a3f2d4
gauzam <gautam01428@gmail.com>
2024-02-02 15:53:43 +0530

    Updated application.properties
    Added properties for Hibernate

--- src/main/java/com/springboot/TeamBook/entity/Employee.java
+++ src/main/java/com/springboot/TeamBook/entity/Employee.java
@@ -19,7 +19,7 @@ public class Employee {
     @Column(name = "last_name")
     private String lastName;
 
-    @Column(name = "email")
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
 
     //define constructors
