85bdba8e88e75a2abdba2eec947a1faf69db76f2
gauzam <gautam01428@gmail.com>
2024-02-09 00:58:54 +0530

    Updated Project Structure

--- teambook-frontend/teambook-frontend/src/components/ListEmployeeComponent.jsx
+++ teambook-frontend/teambook-frontend/src/components/ListEmployeeComponent.jsx
@@ -1,33 +1,20 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import { listEmployees } from '../services/EmployeeService'
 
 const ListEmployeeComponent = () => {
 
-    const dummyData = [
-        {
-            "id": 1,
-            "firstName": "Virat",
-            "lastName": "Kohli",
-            "email": "virat@gmail.com"
-        },
-        {
-            "id": 2,
-            "firstName": "Rohit",
-            "lastName": "Sharma",
-            "email": "rohit@gmail.com"
-        },
-        {
-            "id": 3,
-            "firstName": "Jasprit",
-            "lastName": "Bumrah",
-            "email": "jasprit@gmail.com"
-        },
-        {
-            "id": 4,
-            "firstName": "Shubman",
-            "lastName": "Gill",
-            "email": "shubman@gmail.com"
-        }
-    ]
+    const [employees, setEmployees] = useState([])
+
+    useEffect(() => {
+        listEmployees().then((response) => {
+            setEmployees(response.data);
+        }).catch((error) => {
+            console.error(error);
+        })
+
+    }, [])
+
+
   return (
     <div className='container'>
         
@@ -43,7 +30,7 @@ const ListEmployeeComponent = () => {
             </thead>
             <tbody>
                 {
-                    dummyData.map(employee => 
+                    employees.map(employee => 
                         <tr key = {employee.id}>
                             <td> {employee.id} </td>
                             <td> {employee.firstName} </td>
