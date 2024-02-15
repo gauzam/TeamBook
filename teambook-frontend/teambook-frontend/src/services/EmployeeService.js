a022f5563dd9f9a4eda2bed19f4d12e8ac7e456d
gauzam <gautam01428@gmail.com>
2024-02-16 00:21:16 +0530

    Added Delete Employee Feature

--- teambook-frontend/teambook-frontend/src/services/EmployeeService.js
+++ teambook-frontend/teambook-frontend/src/services/EmployeeService.js
@@ -9,3 +9,5 @@ export const createEmployee = (employee) => axios.post(REST_API_BASE_URL, employ
 export const getEmployee = (employeeID) => axios.get(REST_API_BASE_URL + '/' + employeeID);
 
 export const updateEmployee = (employeeID, employee) => axios.put(REST_API_BASE_URL + '/' + employeeID, employee);
+
+export const deleteEmployee = (employeeID) => axios.delete(REST_API_BASE_URL + '/' + employeeID);
